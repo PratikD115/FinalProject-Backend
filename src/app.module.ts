a6343dafac0cfbabd5d6b7e4996f29974b698b3b
pratik dholariya <dholariyaprateek@gmail.com>
2024-05-06 08:08:51 +0530

    small changes in app module and add the schema file in backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,70 +1,8 @@
-// import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-// import { GraphQLModule } from '@nestjs/graphql';
-// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-// import { AuthModule } from './auth/auth.module';
-// import { UserModule } from './user/user.module';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { ConfigModule } from '@nestjs/config';
-// import { ArtistModule } from './artist/artist.module';
-// import { SongModule } from './song/song.module';
-// import { CloudinaryModule } from './cloudinary/cloudinary.module';
-// import { graphqlUploadExpress } from 'graphql-upload';
-// import { SearchModule } from './search/search.module';
-// import { PlaylistModule } from './playlist/playlist.module';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     MongooseModule.forRoot(
-//       'mongodb+srv://pratik:pratikd98@mernapp.gm42n80.mongodb.net/',
-//     ),
-//     GraphQLModule.forRoot<ApolloDriverConfig>({
-//       driver: ApolloDriver,
-//       playground: true,
-//       autoSchemaFile: true,
-//       useFactory: (config: ConfigService) => {
-//         return {
-//           // ...
-//           formatError: (error) => {
-//             const originalError = error.extensions
-//               ?.originalError as OriginalError;
-
-//             if (!originalError) {
-//               return {
-//                 message: error.message,
-//                 code: error.extensions?.code,
-//               };
-//             }
-//             return {
-//               message: originalError.message,
-//               code: error.extensions?.code,
-//             };
-//           },
-//         };
-//       },
-//     }),
-//     AuthModule,
-//     UserModule,
-//     ArtistModule,
-//     SongModule,
-//     CloudinaryModule,
-//     SearchModule,
-//     PlaylistModule,
-//   ],
-// })
-// export class AppModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(graphqlUploadExpress())
-//       .forRoutes({ path: '/graphql', method: RequestMethod.POST });
-//   }
-// }
-
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigService for factory function
+import { ConfigModule } from '@nestjs/config'; // Import ConfigService for factory function
 import { graphqlUploadExpress } from 'graphql-upload';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
